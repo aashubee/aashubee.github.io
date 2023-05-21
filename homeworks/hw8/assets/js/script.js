3baff7eb776d646e5ca5916534cf47b3ef9b6ca5
aashubee <aashubee@gmail.com>
2023-05-21 03:36:14 -0700

    'website'

--- homeworks/hw8/assets/js/script.js
+++
@@ -1,5 +0,0 @@
-function toggleSongs(){
-    songsSecondHalf = document.getElementById("songs-second-half")
-    songsSecondHalf.classList.toggle("hidden");
-}
-document.getElementById("toggle-button").onclick = toggleSongs;
